223e081eaa4e834c4814221aad23b08519be2f8e
Nikita Balin <business.nikitap@gmail.com>
2023-07-20 19:54:22 +0600

    Update fileUpload.utils.ts

--- src/utils/fileUpload.utils.ts
+++ src/utils/fileUpload.utils.ts
@@ -1,10 +1,21 @@
 import { extname } from 'path';
 import generateRandomName from './generateRandomName.utils';
+import Errors from 'src/errors.enum';
+import { HttpException } from '@nestjs/common';
 
 export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|svg|png|webp)$/)) {
-    return callback(new Error('Only image files are allowed!'), false);
-  }
+  if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|webp)$/) || !req)
+    return callback(
+      new HttpException(
+        {
+          status: false,
+          ...Errors.ONLY_IMAGES_ARE_ALLOWED,
+        },
+        200,
+      ),
+      false,
+    );
+
   callback(null, true);
 };
 
